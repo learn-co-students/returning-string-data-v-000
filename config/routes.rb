c08a5f0356bf42b40a3db5b3b925daad961d3ec4
Autumn Kangas <autumnkangas@gmail.com>
2018-03-26 23:44:49 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   resources :posts, only: [:index, :show, :new, :create, :edit]
+
+  #power AJAX request to replace truncated content on /posts with full body of post
+  get '/posts/:id/body', to: 'posts#body'
 end
