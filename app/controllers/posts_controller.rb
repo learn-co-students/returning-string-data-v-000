ac26434dd6f15c5fcce13e652551ad9067574bc4
Robert Hughes <roberts.ginger.email@gmail.com>
2018-06-17 07:05:21 +0100

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,11 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show, :edit, :update]
+  before_action :set_post, only: %i[show edit update]
 
   def index
     @posts = Post.all
   end
 
-  def show
-  end
+  def show; end
 
   def new
     @post = Post.new
@@ -18,21 +17,24 @@ class PostsController < ApplicationController
     redirect_to post_path(@post)
   end
 
-  def edit
-  end
+  def edit; end
 
   def update
     @post.update(post_params)
     redirect_to post_path(@post)
   end
 
-private
-  # Use callbacks to share common setup or constraints between actions.
+  def body
+    post = Post.find(params[:id])
+    render plain: post.description
+  end
+
+  private
+
   def set_post
     @post = Post.find(params[:id])
   end
 
-  # Never trust parameters from the scary internet, only allow the white list through.
   def post_params
     params.require(:post).permit(:title, :description)
   end
