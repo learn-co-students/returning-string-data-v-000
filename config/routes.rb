0ca17e9b27f927ff2bd96da7bbe4bfcecffd3dc1
AspenJames <aspenjames@tqca.org>
2018-07-29 15:32:26 -0700

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   resources :posts, only: [:index, :show, :new, :create, :edit]
+
+  patch '/posts/:id' => 'posts#update'
+
+  get '/posts/:id/body' => 'posts#body'
 end
