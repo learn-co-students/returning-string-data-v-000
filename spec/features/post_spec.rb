faf4d25dee6aaca01dfd240ee2860115340970d1
Lara Sanders <larasanders@gmail.com>
2018-08-02 11:54:15 +0000

    Done.

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -19,19 +19,19 @@ describe 'navigate' do
     visit post_path(@post)
     expect(page).to have_css("p", text: "My post desc")
   end
-# end
+end
 
-# describe 'form' do
-#   it 'shows a new form that submits content and redirects and prints out params' do
-#     visit new_post_path
-#
-#     fill_in 'title', with: "My post title"
-#     fill_in 'description', with: "My post description"
-#
-#     click_on "Submit Post"
-#
-#     expect(page).to have_content("My post title")
-  #  end
+describe 'form' do
+  it 'shows a new form that submits content and redirects and prints out params' do
+    visit new_post_path
+
+    fill_in 'post[title]', with: "My post title"
+    fill_in 'post[description]', with: "My post description"
+
+    click_on "Submit Post"
+
+    expect(page).to have_content("My post title")
+   end
 
   it 'shows a new form that submits content and redirects and prints out params' do
     @post = Post.create(title: "My Post", description: "My post desc")
