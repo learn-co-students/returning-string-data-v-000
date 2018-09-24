9742bf6096777fc1b99a0718606fb4b7dec06c9b
github username <github email address>
2018-09-24 13:04:07 -0500

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,6 +6,7 @@ class PostsController < ApplicationController
   end
 
   def show
+    @post= Post.find(params[:id])
   end
 
   def new
@@ -19,6 +20,7 @@ class PostsController < ApplicationController
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   def update
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   resources :posts, only: [:index, :show, :new, :create, :edit]
 
+patch 'posts/:id', to: 'posts#update'
+
+
   get '/posts/:id/body', to: 'posts#body'
 end
