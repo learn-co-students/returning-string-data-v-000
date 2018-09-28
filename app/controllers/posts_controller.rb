d81f74d848590ff55b95a39b17421073f2cc333b
Courtney <admin@Courtneys-Macbook-Air-2.local>
2018-09-28 10:08:42 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,11 +19,17 @@ class PostsController < ApplicationController
   end
 
   def edit
+    @edit_post = "Yes"
   end
 
   def update
     @post.update(post_params)
     redirect_to post_path(@post)
+  end
+
+  def body
+    post = Post.find(params[:id])
+    render plain: post.description
   end
 
 private
