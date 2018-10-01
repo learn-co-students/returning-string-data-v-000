2291cbeb801d09a946485e0fcca291b459b529b9
Aubrey Neimeier <neimeier.1@osu.edu>
2018-10-01 20:30:39 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,9 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update]
+  def body
+      post = Post.find(params[:id])
+      render plain: post.description
+    end
 
   def index
     @posts = Post.all
