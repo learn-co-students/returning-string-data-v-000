fb4df47e03e716b9350d2f61e2aaac51dbcd6290
wendycalderon@gmail.com <wendycalderon@gmail.com>
2019-06-21 13:54:54 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   resources :posts, only: [:index, :show, :new, :create, :edit]
+
+  #route and controller action to get the body of the post:
+  get '/posts/:id/body', to: 'posts#body'
 end
