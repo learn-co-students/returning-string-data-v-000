91923a6ebac554cf45b72ea04179d23428f43712
sjwakean <swakeman@ca.rr.com>
2019-08-03 17:43:37 -0700

    Done.

--- db/migrate/20160119185429_add_author_id_to_posts.rb
+++ db/migrate/20160119185429_add_author_id_to_posts.rb
@@ -1,5 +1,5 @@
 class AddAuthorIdToPosts < ActiveRecord::Migration
-  def change
-    add_column :posts, :author_id, :integer
+    def change
+      add_column :posts, :author_id, :integer
+    end
   end
-end
